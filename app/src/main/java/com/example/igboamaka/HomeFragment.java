710c08894e5c2c05b2ed5d0058cdacfb091fa7f6
Sommie09 <ceekay2a7@gmail.com>
2020-07-18 23:30:15 +0100

    Removed choose level activity

--- app/src/main/java/com/example/igboamaka/HomeFragment.java
+++ app/src/main/java/com/example/igboamaka/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.igboamaka;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,8 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.igboamaka.numbers.NumbersActivity;
+import com.google.android.material.card.MaterialCardView;
+
 
 public class HomeFragment extends Fragment {
+    MaterialCardView numbersCard;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -21,6 +26,15 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        numbersCard = (MaterialCardView) rootView.findViewById(R.id.numbers_card);
+        numbersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), NumbersActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
---
+++ app/src/main/java/com/example/igboamaka/numbers/OneToTenFragment.java
@@ -0,0 +1,28 @@
+package com.example.igboamaka.numbers;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.igboamaka.R;
+
+
+public class OneToTenFragment extends Fragment {
+
+
+    public OneToTenFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one_to_ten, container, false);
+    }
+}
